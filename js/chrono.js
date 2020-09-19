a956c4c88b4760b5cf4436f2acbf40f1004fd8c3
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-19 09:26:33 +0200

    Affichage hhmmss

--- js/chrono.js
+++ js/chrono.js
@@ -1,6 +1,6 @@
 let heures = document.querySelector(".heures>span");
 let minutes = document.querySelector(".minutes>span");
 let secondes = document.querySelector(".secondes>span");
-heures.textContent = "00";
-minutes.textContent = "00";
-secondes.textContent = "00";
+heures.textContent = "00h";
+minutes.textContent = "00m";
+secondes.textContent = "00s";
