56b3a9bbb980e7c308ea4e948f3a7d32aa779978
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-19 09:22:12 +0200

    Mise en forme et début du JS

---
+++ js/chrono.js
@@ -0,0 +1,6 @@
+let heures = document.querySelector(".heures>span");
+let minutes = document.querySelector(".minutes>span");
+let secondes = document.querySelector(".secondes>span");
+heures.textContent = "00";
+minutes.textContent = "00";
+secondes.textContent = "00";
